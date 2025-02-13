3c16abef59a7c8259e095df0ea2556f412440ac5
Muhammad Fahad <mistr.faha06@gmail.com>
2025-02-13 17:33:43 +0500

    build

--- src/app/privacy-policy/page.js
+++ src/app/privacy-policy/page.js
@@ -2,6 +2,7 @@ import { Container } from 'react-bootstrap'
 
 const Page = () => {
     return (
+
         <>
             <Container fluid='sm'>
                 <section className=' sm:mt-6 pt-3'>
